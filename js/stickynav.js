065060c935dce1862b9dd2f9aa698417eb617ab5
xti28 <cagg@hawaii.edu>
2020-04-01 03:12:30 -1000

    init

---
+++ js/stickynav.js
@@ -0,0 +1,16 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    //STICKY NAV LINKS TOGGLE MENU NAV
+    var stickynavlinks = document.querySelectorAll(".sticky nav a");
+    var j;
+    for (j = 0; j < stickynavlinks.length; j++) {
+        stickynavlinks[j].onclick = function () {
+            var stickymenu = document.querySelectorAll(".sticky .site-navigation");
+            var k;
+            for (k = 0; k < stickymenu.length; k++) {
+                stickymenu[k].classList.remove("expanded");
+            }
+        };
+    }
+});
